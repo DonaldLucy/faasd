40fb25a6280cfc221ad84cb4642a869add385e01
Daniel Xiong <daniel@daniels-mbp-64.mynetworksettings.com>
2025-12-11 13:35:06 -0500

    updated junctiond. Fixed some proto errors (still buggy)

---
+++ junctiond/build/CMakeFiles/junctiond.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/junctiond.dir/gen/junctiond.grpc.pb.cc.o"
+  "CMakeFiles/junctiond.dir/gen/junctiond.grpc.pb.cc.o.d"
+  "CMakeFiles/junctiond.dir/gen/junctiond.pb.cc.o"
+  "CMakeFiles/junctiond.dir/gen/junctiond.pb.cc.o.d"
+  "CMakeFiles/junctiond.dir/junctiond.cpp.o"
+  "CMakeFiles/junctiond.dir/junctiond.cpp.o.d"
+  "CMakeFiles/junctiond.dir/junctiond_server.cpp.o"
+  "CMakeFiles/junctiond.dir/junctiond_server.cpp.o.d"
+  "junctiond"
+  "junctiond.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/junctiond.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
